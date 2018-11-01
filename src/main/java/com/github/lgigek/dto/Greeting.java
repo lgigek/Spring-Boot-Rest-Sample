7ecad512999012db4974bcb7a91c031a1cad987f
Lucas Gigek <lucasgigek@gmail.com>
2018-11-01 17:40:43 -0300

    added Data annotation to Greeting

--- src/main/java/com/github/lgigek/dto/Greeting.java
+++ src/main/java/com/github/lgigek/dto/Greeting.java
@@ -1,9 +1,8 @@
 package com.github.lgigek.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
-//@Data
+@Data
 public class Greeting {
     private final long id;
     private final String content;
@@ -11,14 +10,6 @@ public class Greeting {
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
     }
 
 }
